124e5b61798417306ce12ee7718417da1be7e59c
Anderson Amorim <andersonamorim939@gmail.com>
2023-03-23 17:40:18 -0300

    remove build

--- backend-desafio/build/src/models/counters.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const CounterSchema = new mongoose_1.Schema({
-    seq: { type: Number, default: 1 },
-    nameCounter: { type: String, require: true }
-});
-exports.default = (0, mongoose_1.model)('counter', CounterSchema, 'counter');
